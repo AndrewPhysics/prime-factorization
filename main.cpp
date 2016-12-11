8123d31d4a4590a4a91b2b53ca33b199b4a6b5c3
ANDRAS SZABO <szabo.andrew@yahoo.com>
2016-12-11 23:43:03 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+
+bool P_property(int x) {
+	return x == 0;
+}
+
+
+
+void getPrimeFactors(int aa[], int m, int number) {
+	cout << "the prime factorization of number=" << number << " is: " << endl;
+	
+	cout << number << "=";
+	for (int i = 0; i < m; i++)
+	{
+		cout<< aa[i] << "*";
+	}
+	cout << "1" << endl;
+}
+void factor1() {
+	int number;
+	cout << "Please type in an integer number!" << endl;
+	cin >> number;
+	int numberOut = number;
+	int divisor = 2;
+	//Suppose that the number of prime factors are less then 10
+	int aa[10] = { 0,0,0,0,0,0,0,0,0,0 };
+	int i = 0;
+	while (number>1)
+	{
+		if (number % divisor == 0)
+		{
+			cout << number << "|" << divisor << endl;
+			
+			number = number / divisor;
+			aa[i] = divisor;
+			i++;
+		}
+		else
+		{
+			divisor += 1;
+		}
+	}
+	cout << '1' << endl;
+	cout << "---------------------" << endl;
+	getPrimeFactors(aa, i, numberOut);
+}
+
+
+
+int main() {
+	factor1();
+	factor1();
+
+	system("pause");
+	return 0;
+}
